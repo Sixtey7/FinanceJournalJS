8f06705d60e81b81c3285d646b64c1720bb11fb5
Sixtey7 <sixtey7@gmail.com>
2016-05-01 11:53:08 +0000

    expand the ugly ui, only store date (not time) in the most hacky way possible)

--- app/models/entry.server.model.js
+++ app/models/entry.server.model.js
@@ -6,10 +6,20 @@ var EntrySchema = new Schema({
   amount : Number,
   date : {
     type : Date,
-    default : Date.now
   },
   estimate : Boolean,
   notes : String
+});
+
+EntrySchema.pre('save', function(next) {
+  if (!this.date) {
+    this.date = Date.now();
+  }
+  this.date.setHours(0);
+  this.date.setMinutes(0);
+  this.date.setSeconds(0);
+  this.date.setMilliseconds(0);
+  next();
 });
 
 mongoose.model('Entry', EntrySchema);
