1c8de290094394d8401b519ee3ebd568d770c35a
Sixtey7 <sixtey7@gmail.com>
2016-05-02 11:12:14 +0000

    added an endpoint to take in a csv and create entries out of it

--- app/routes/entry.server.routes.js
+++ app/routes/entry.server.routes.js
@@ -1,4 +1,6 @@
 var entry = require('../../app/controllers/entry.server.controller');
+var bodyParser = require('body-parser');
+
 require('../../config/colors');
 
 module.exports = function(app) {
@@ -10,6 +12,9 @@ module.exports = function(app) {
     .get(entry.read)
     .put(entry.update)
     .delete(entry.delete);
+
+  app.route('/api/entries/load')
+    .post(bodyParser.text(), entry.createFromCSV);
 
   app.param('entryId', entry.entryById);
 }
