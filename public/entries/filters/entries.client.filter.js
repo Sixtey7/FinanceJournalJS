70864b548c42f84f7c78c5b261b4e92ae5cfedd3
root <root@DESKTOP-5QEO9TK.localdomain>
2016-05-08 02:24:15 +0000

    material everything!

--- public/entries/filters/entries.client.filter.js
+++ public/entries/filters/entries.client.filter.js
@@ -10,10 +10,25 @@ module.filter('abs', function() {
 });
 
 /**
+* Filter used to format currency to two decimal places
+**/
+module.filter('currency', function() {
+  return function(input) {
+    if (input) {
+      return input.toFixed(2);
+    }
+    else {
+      return "";
+    }
+  }
+})
+
+/**
 * Filter used to make the date prettier on the ui
 **/
 module.filter('formatDate', function() {
   return function(date) {
+    console.log('Formatting Date...');
     if (date) {
       //TODO: May want to clean this up a bit to make the date even prettier
       var splitResults = date.split('T');
